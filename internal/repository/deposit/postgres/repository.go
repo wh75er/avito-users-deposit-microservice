5e351fdab43c277b6726093d9b92a4e9fd377c9e
wh75er <wh75er@gmail.com>
2021-09-09 17:20:32 +0300

    add postgres repositories implementations

---
+++ internal/repository/deposit/postgres/repository.go
@@ -0,0 +1,83 @@
+package postgres
+
+import (
+	"bank-microservice/internal/errors"
+	"bank-microservice/internal/models"
+	"database/sql"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+)
+
+type DepositRepository struct {
+	Db *sqlx.DB
+	Logger *logrus.Logger
+}
+
+func New(db *sqlx.DB, logger *logrus.Logger) models.DepositRepository {
+	return &DepositRepository{ db, logger }
+}
+
+
+func (r *DepositRepository) GetDepositIdByOwner(ownerUuid uuid.UUID) (id int64, e error) {
+	e = r.Db.Get(&id, "SELECT id FROM deposits WHERE userUuid == $1", ownerUuid)
+	if e == sql.ErrConnDone {
+		e = errors.E(errors.RepositoryDownErr, e)
+	} else if e == sql.ErrNoRows {
+		e = errors.E(errors.RepositoryNoRows, e)
+	} else if e != nil {
+		e = errors.E(errors.RepositoryQueryErr)
+	}
+
+	return
+}
+
+func (r *DepositRepository) GetDepositByOwner(ownerUuid uuid.UUID) (d models.Deposit, e error) {
+	e = r.Db.Get(&d, "SELECT id, userUuid, deposit, creationDate FROM deposits WHERE userUuid == $1", ownerUuid)
+	if e == sql.ErrConnDone {
+		e = errors.E(errors.RepositoryDownErr, e)
+	} else if e == sql.ErrNoRows {
+		e = errors.E(errors.RepositoryNoRows, e)
+	} else if e != nil {
+		e = errors.E(errors.RepositoryQueryErr)
+	}
+
+	return
+}
+
+func (r *DepositRepository) GetDepositById(id int64) (d *models.Deposit, e error) {
+	e = r.Db.Get(&d, "SELECT id, userUuid, deposit, creationDate FROM deposits WHERE id == $1", id)
+	if e == sql.ErrConnDone {
+		e = errors.E(errors.RepositoryDownErr, e)
+	} else if e == sql.ErrNoRows {
+		e = errors.E(errors.RepositoryNoRows, e)
+	} else if e != nil {
+		e = errors.E(errors.RepositoryQueryErr)
+	}
+
+	return
+}
+
+func (r *DepositRepository) AddNewDepositForOwner(d *models.Deposit) (id int64, e error) {
+	e = r.Db.QueryRowx("INSERT INTO deposits(userUuid, deposit, creationDate) VALUES ($1, $2, $3) RETURNING id",
+		d.UserUuid, d.Deposit, d.CreationDate).Scan(&id)
+	if e == sql.ErrConnDone {
+		e = errors.E(errors.RepositoryDownErr, e)
+	} else if e != nil {
+		e = errors.E(errors.RepositoryQueryErr, e)
+	}
+
+	return
+}
+
+func (r *DepositRepository) UpdateDepositByOwner(d *models.Deposit) (e error) {
+	_, e = r.Db.Exec("UPDATE deposits SET deposit = $1 WHERE userUuid = $2",
+		d.Deposit, d.UserUuid)
+	if e == sql.ErrConnDone {
+		e = errors.E(errors.RepositoryDownErr, e)
+	} else if e != nil {
+		e = errors.E(errors.RepositoryQueryErr, e)
+	}
+
+	return
+}
---
+++ internal/repository/transaction/postgres/repository.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"bank-microservice/internal/errors"
+	"bank-microservice/internal/models"
+	"database/sql"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+)
+
+type TransactionRepository struct {
+	Db *sqlx.DB
+	Logger *logrus.Logger
+}
+
+func New(db *sqlx.DB, logger *logrus.Logger) models.TransactionRepository {
+	return &TransactionRepository{ db, logger }
+}
+
+func (r *TransactionRepository) AddTransaction(t *models.Transaction) (e error) {
+	_, e = r.Db.Exec("INSERT INTO Transactions (DepositId, OwnerUuid, Amount, Reason, PartnerUuid, TransactionDate) " +
+		"VALUES($1, $2, $3, $4, $5, $6)", t.DepositId, t.OwnerUuid, t.Amount, t.Reason, t.PartnerUuid, t.TransactionDate)
+	if e == sql.ErrConnDone {
+		e = errors.E(errors.RepositoryDownErr, e)
+	} else if e != nil {
+		e = errors.E(errors.RepositoryQueryErr, e)
+	}
+
+	return
+}
